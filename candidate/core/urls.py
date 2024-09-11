1ab00ae2bbe1287ed733a145c0f8a779cea911e8
Shayne999 <shaynejuniorn@gmail.com>
2024-09-11 17:18:07 +0200

    Basic Recruiter dashboard functionality handled

--- candidate/core/urls.py
+++ candidate/core/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('candidate_dashboard/', views.candidate_dashboard, name='candidate_dashboard'),
     path('recruiter_dashboard/', views.recruiter_dashboard, name='recruiter_dashboard'),
     path('edit_cv/', views.edit_cv, name='edit_cv'),
+    path('candidate_cv/<int:candidate_id>/', views.candidate_cv, name='candidate_cv'),
     path('logout/', views.logout, name='logout'),
 ]
